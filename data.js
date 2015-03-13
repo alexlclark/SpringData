0650dec12d6183b657dbc6d3f955e862b6b50fc7
alexlclark <alexclarkcontact@gmail.com>
2015-03-12 23:55:47 -0400

    renamed var in data to albumdata

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
-var jsonFREDData = { //heres the big json dump from FRED 
-    "observations": [{"Album":1,"Days From Release":8},
-{"Album":2,"Days From Release":11},
-{"Album":3,"Days From Release":12},
-{"Album":4,"Days From Release":15},
-{"Album":5,"Days From Release":13},
-{"Album":6,"Days From Release":23},
-{"Album":7,"Days From Release":9},
-{"Album":8,"Days From Release":11},
-{"Album":9,"Days From Release":11},
-{"Album":10,"Days From Release":21},
-{"Album":11,"Days From Release":12},
-{"Album":12,"Days From Release":21}]	
+var albumData = { //heres the big json dump from FRED 
+    "observations": [{"Album":1,"Days":8},
+{"Album":2,"Days":11},
+{"Album":3,"Days":12},
+{"Album":4,"Days":15},
+{"Album":5,"Days":13},
+{"Album":6,"Days":23},
+{"Album":7,"Days":9},
+{"Album":8,"Days":11},
+{"Album":9,"Days":11},
+{"Album":10,"Days":21},
+{"Album":11,"Days":12},
+{"Album":12,"Days":21}]
 }
