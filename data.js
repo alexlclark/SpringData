025a61759f4164b27dd00cc268375aa7c3cb630f
alexlclark <alexclarkcontact@gmail.com>
2015-03-13 01:11:10 -0400

    created all new data with new field names

--- data.js
+++ data.js
@@ -1,14 +1,12 @@
 var albumData = { //heres the big json dump from FRED 
-    "observations": [{"Album":1,"Days":8},
-{"Album":2,"Days":11},
-{"Album":3,"Days":12},
-{"Album":4,"Days":15},
-{"Album":5,"Days":13},
-{"Album":6,"Days":23},
-{"Album":7,"Days":9},
-{"Album":8,"Days":11},
-{"Album":9,"Days":11},
-{"Album":10,"Days":21},
-{"Album":11,"Days":12},
-{"Album":12,"Days":21}]
+    "observations": [{"chartPos":1,"albumName":"Frozen","albumArtist":"Soundtrack","album":"Soundtrack - Frozen","releaseDate":"11/25/13","waffleDate":"12/2/13","daysDiff":7,"daysDiffMaxSev":7},
+{"chartPos":2,"albumName":"Beyonce","albumArtist":"Beyonce","album":"Beyonce - Beyonce","releaseDate":"12/13/13","waffleDate":"12/20/13","daysDiff":7,"daysDiffMaxSev":7},
+{"chartPos":3,"albumName":"1989","albumArtist":"Taylor Swift","album":"Taylor Swift - 1989","releaseDate":"11/27/14","waffleDate":"11/24/14","daysDiff":-3,"daysDiffMaxSev":-3},
+{"chartPos":4,"albumName":"Midnight Memories","albumArtist":"One Direction","album":"One Direction - Midnight Memories","releaseDate":"11/25/13","waffleDate":"11/18/13","daysDiff":-7,"daysDiffMaxSev":-7},
+{"chartPos":5,"albumName":"The Marshall Mathers LP 2","albumArtist":"Eminem","album":"Eminem - The Marshall Mathers LP 2","releaseDate":"9/20/13","waffleDate":"10/31/13","daysDiff":41,"daysDiffMaxSev":7},
+{"chartPos":6,"albumName":"Pure Heroine","albumArtist":"Lorde","album":"Lorde - Pure Heroine","releaseDate":"9/27/13","waffleDate":"9/26/13","daysDiff":-1,"daysDiffMaxSev":-1},
+{"chartPos":7,"albumName":"Crash My Party","albumArtist":"Luke Bryan","album":"Luke Bryan - Crash My Party","releaseDate":"8/13/13","waffleDate":"8/13/13","daysDiff":0,"daysDiffMaxSev":0},
+{"chartPos":8,"albumName":"Prism","albumArtist":"Katy Perry","album":"Katy Perry - Prism","releaseDate":"11/17/13","waffleDate":"11/17/13","daysDiff":0,"daysDiffMaxSev":0},
+{"chartPos":9,"albumName":"Blame It All On My Roots: Five Decades Of Influences","albumArtist":"Garth Brooks","album":"Garth Brooks - Blame It All On My Roots: Five Decades Of Influences","releaseDate":"11/28/13","waffleDate":"3/14/14","daysDiff":106,"daysDiffMaxSev":7},
+{"chartPos":10,"albumName":"Here's To The Good Times","albumArtist":"Florida Georgia Line","album":"Florida Georgia Line - Here's To The Good Times","releaseDate":"12/4/12","waffleDate":"12/6/12","daysDiff":2,"daysDiffMaxSev":2}]
 }
