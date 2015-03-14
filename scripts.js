e1df53feba748ab728afe1d6e986acd211a00d42
alexlclark <alexclarkcontact@gmail.com>
2015-03-13 21:16:07 -0400

    changed color of bars to orange. experimented with different legend positions, decided to remove it

--- scripts.js
+++ scripts.js
@@ -25,9 +25,9 @@ function drawChart() {
 		title : 'Time between album release and pirated, private BitTorrent tracker release',
 		legend : {
 			position : 'none'
-		},
-		 hAxis: { title: "Days from album release to torrent appearance on waffles.fm", textStyle: {color: '#fcd116', fontName: 'arial', fontSize: 10} },
- vAxis: { title: "Number of albums in range", textStyle: {color: '#fcd116', fontName: 'arial', fontSize: 10} },
+		},colors: ['#e7711c'],
+		 hAxis: { title: "Days from album release to torrent appearance on waffles.fm", textStyle: {color: '#0673b2', fontName: 'arial', fontSize: 10} },
+ vAxis: { title: "Number of albums in range", textStyle: {color: '#0673b2', fontName: 'arial', fontSize: 10} },
 	};
 
 	var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
@@ -44,53 +44,3 @@ else {
 }
 }
 
-/*
- <!--
- var myTextStyle = {
- color: 'yellow',
- fontName: 'arial',
- fontSize: 10,};
-
- google.load('visualization', '1.0', {'packages':['corechart']});
-
- // Set a callback to run when the Google Visualization API is loaded.
- google.setOnLoadCallback(drawChart);
-
- // Callback that creates and populates a data table,
- // instantiates the pie chart, passes in the data and
- // draws it.
- function drawChart() {
-
- //NEW - Use a for loop to  make an array of arrays out of relevant JSON data
- var arraysFat = [] //created big empty array to hold smaller date/val arrays0
-
- for (var i=0;/*spacesdont  matter in for loopsi<albumData.observations.length; i++){ //all for loops ever will appear like this. start, length,
-
- var itemArray=[];
- itemArray.push(albumData.observations[i].album);//adding date and value to item array (the small array)
- itemArray.push( Number(albumData.observations[i].daysDiffMaxSev)); //noted itemarray as number
-
- arraysFat.push(itemArray); //add my little array to the larger one, arraysFat
-
- }
- // Create the data table.
- var data = new google.visualization.DataTable();
- data.addColumn('string', 'album');
- data.addColumn('number', 'daysDiffMaxSev');
- data.addRows(arraysFat); // this has to equal the array at the top (the fat one)
-
- // Set chart options
- var options = {'title':'Litres of Vodka Exported by Year',
- 'width':800,
- 'height':1700,
- hAxis: { gridlines: {color: '#fcd116', count: 4} },
- vAxis: { textStyle: {color: '#fcd116', fontName: 'arial', fontSize: 10} },
- backgroundColor: "transparent",};
-
- options.hAxis.textStyle = myTextStyle;
- console.log(options.height)
-
- // Instantiate and draw our chart, passing in some options.
- var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
- chart.draw(data, options);
- }-->*/
